54f221005276a5567e54f0b2d2f6670997bb7e97
Matias Perez Ferrero <matias@Matiass-MacBook-Pro.local>
2020-02-12 12:54:09 -0700

    finished lecture

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
+// require('dotenv').config({path: __dirname + '/../.env'}) -> for hosting reference
+require('dotenv').config()
 const express = require("express");
+const massive = require('massive')
+const controller = require('./controller')
 
 const app = express();
 
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+console.log('SERVER_PORT:', SERVER_PORT, 'CONNECTION:', CONNECTION_STRING)
+
+massive(CONNECTION_STRING).then(dbObj => {
+  app.set('db', dbObj)
+  //app = {
+  //   db: dbObj
+  // }
+
+  
+  // dbInstance.new_planes().then(
+  //   planes => console.log(planes)
+  // ).catch(err => console.log(err))
+
+})
 
 app.use(express.json());
+
+app.get('/api/planes', controller.getPlanes)
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server listening on port ${SERVER_PORT}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
